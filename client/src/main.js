1b1ecaeb9c1e8b89db83862e5cbddf9957f3a8a3
az-m <alisonzmills@gmail.com>
2025-05-28 15:11:54 +0100

    feat: get tag info from db

--- client/src/main.js
+++ client/src/main.js
@@ -33,3 +33,46 @@ document.getElementById("sideMenuBtn").addEventListener("click", () => {
 document.getElementById("closeMenuBtn").addEventListener("click", () => {
   document.getElementById("sideMenu").style.width = "0px";
 });
+
+filmTagArray();
+gameTagArray();
+foodTagArray();
+
+// get the film tag data
+
+async function filmTagArray() {
+  const data = await getCategoryData("get_films");
+  const tags = getTags(data);
+  console.log(tags);
+}
+
+// get the games tag data
+
+async function gameTagArray() {
+  const data = await getCategoryData("get_games");
+  const tags = getTags(data);
+  console.log(tags);
+}
+
+// get the food tag data
+
+async function foodTagArray() {
+  const data = await getCategoryData("get_food");
+  const tags = getTags(data);
+  console.log(tags);
+}
+
+async function getCategoryData(cat) {
+  const API = `http://localhost:8080/` + cat;
+  const response = await fetch(API);
+  const data = await response.json();
+  return data;
+}
+
+function getTags(data) {
+  let tagArray = [];
+  data.forEach((item) => {
+    tagArray = [...new Set([...tagArray, ...item.tag])];
+  });
+  return tagArray;
+}
